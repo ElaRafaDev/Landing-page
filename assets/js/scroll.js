8329f516a81f520a189783a7bf423827c498c09e
Rafaela <study.program67@gmail.com>
2022-01-20 14:00:30 -0400

    .
    .

--- assets/js/scroll.js
+++
@@ -1,9 +0,0 @@
-const scrollA = document.querySelector('[data-anima="scroll"]');
-
-document.addEventListener('scroll', function(e) {
-  if (window.scrollY > 664) {
-    scrollA.classList.add("show-button");
-  } else {
-    scrollA.classList.remove("show-button");
-  }
-});
